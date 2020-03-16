b619772c9dee9e0aaeb246662ce6804f18971c70
matiaet98 <matias.exequiel.estevez@gmail.com>
2020-03-16 15:11:55 -0300

    add cors middleware

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const compression = require('compression')
 const helmet = require('helmet')
+const cors = require('cors')
 
 const testRouter = require('./routes/test')
 
 const app = express()
 
+app.use(helmet())
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(compression())
-app.use(helmet())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
