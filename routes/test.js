6ba17700b9c69447e7bb0d4ac18ca3c9388b370f
matiaet98 <matias.exequiel.estevez@gmail.com>
2020-05-18 21:01:57 -0300

    add auth information

--- routes/test.js
+++ routes/test.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const { Router } = require('express');
 const router = express.Router();
 
 /* GET send test response. */
 router.get('/', function(req, res, next) {
+  console.log(req)
   res.json({
     response: "hello world",
     api: "expressjs",
-    version: "1"
+    version: "1",
+    auth: req.headers.authorization
   })
 });
 
